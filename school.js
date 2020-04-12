56575067ffbe3e7c637d6ba81b467fd0700f371a
Michael Michaud <mmichaud3@yahoo.com>
2020-04-12 14:45:11 -0700

    message_app

---
+++ school.js
@@ -0,0 +1,61 @@
+class School {
+    constructor(name, level, numberOfStudents) {
+      this._name = name;
+      this._level = level;
+      this._numberOfStudents = numberOfStudents;
+    }
+    get name() {
+      return this._name;
+    }
+    get level() {
+      return this._level;
+    }
+    get numberOfStudents() {
+      return this._numberOfStudents;
+    }
+    set numberOfStudents(newNumber) {
+      if (typeof numberOfStudents === "number") {
+        this._numberOfStudents = numberOfStudents;
+      } else {
+        console.log('Invalid input: numberOfStudents must be set to a Number.');
+      }
+    }
+    quickFacts() {
+      console.log(`${this.name} educates ${this.numberOfStudents} students at the ${this.level} school level.`);
+    }
+    static pickSubstituteTeacher(substituteTeachers) {
+      let num = Math.floor(substituteTeachers.length * Math.random());
+      return substituteTeachers[num];
+    }
+  }
+  class PrimarySchool extends School {
+    constructor(name, numberOfStudents, pickUpPolicy) {
+      super(name, 'primary', numberOfStudents);
+      this._pickUpPolicy = pickUpPolicy;
+    }
+    get pickUpPolicy() {
+      return this._pickUpPolicy;
+    }
+  }
+  class HighSchool extends School {
+    constructor(name, numberOfStudents, sportsTeams) {
+      super(name, 'high', numberOfStudents);
+      this._sportsTeams = sportsTeams;
+    }
+    get sportsTeams() {
+      console.log(this._sportsTeams);
+    }
+  }
+  
+  const lorraineHansbury = new PrimarySchool('Larraine Hansbury', 514, 'Students must be picked up by a parent, guardian, or a family member over the age of 13.');
+  
+  lorraineHansbury.quickFacts();
+  
+  School.pickSubstituteTeacher(['Jamal Crawford', 'Lou Williams', 'J. R. Smith', 'James Harden', 'Jason Terry', 'Manu Ginobli']);
+  
+  const alSmith = new HighSchool('Al E. Smith', 415,['Baseball', 'Basketball', 'Volleyball', 'Track and Field'] );
+  
+  console.log(alSmith.sportsTeams);
+  
+  
+  
